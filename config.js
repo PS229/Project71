2c09b81415b1b6507984d62a0010dc6fe45f3f48
PS229 <84014874+PS229@users.noreply.github.com>
2021-08-29 16:16:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+ //Add configuration here
+ var firebaseConfig = {
+  apiKey: "AIzaSyB2REEKlBTDgpYrFDsNo6eb10299a98RIk",
+  authDomain: "project-71-d4574.firebaseapp.com",
+  projectId: "project-71-d4574",
+  storageBucket: "project-71-d4574.appspot.com",
+  messagingSenderId: "946045236626",
+  appId: "1:946045236626:web:f0cd3d8d5ad4ce3cb0bd4b"
+};
+};
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
